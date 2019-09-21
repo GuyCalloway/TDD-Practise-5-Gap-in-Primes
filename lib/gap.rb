88b8f811b599d37086d793c628c1deb48bf848b0
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-21 16:25:00 +0100

    Final refactored optimised version, passes Codewars tests also

--- lib/gap.rb
+++ lib/gap.rb
@@ -14,49 +14,21 @@ def prime_check(num)
     (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
   end
 
-  #ASK WHY THIS IS SO MUCH FASTER^^
-# def prime_check(x)
-#     return if x <= 1
-#     arr = [*2..Math.sqrt(x)]
-#     arr.none? {|z| if x % z == 0
-                  
-#     end}
-# end
-
-# def gap_1(g, m, n)
-    
-#     x = m
-#     arr_primes = []
-#     until x == n + 1
-#         if prime_check(x)
-#             arr_primes << x
-#             if arr.length != 1
-#               if arr_primes[1] - arr_primes[0] == g
-#                 return arr_primes
-#               end
-#                arr_primes.shift
-#             end
-#         end
-#         x += 1
-#     end
-#     return nil
-# end
-
 def gap_1(g, m, n)
-
-    arr_of_primes = [*m..n].select { |x| prime_check(x)}     
-    i = 0
-    j = 1
-
-    until arr_of_primes[j] == nil
-        if g == arr_of_primes[j] - arr_of_primes[i]
-            return [arr_of_primes[i],arr_of_primes[j]]
+    x = m
+    arr_primes = []
+    
+    until x == n + 1
+        if prime_check(x)
+            arr_primes << x
+            if arr_primes.length != 1
+              if arr_primes[1] - arr_primes[0] == g
+                return arr_primes
+              end
+               arr_primes.shift
+            end
         end
-        i += 1
-        j += 1
+        x += 1
     end
-    
     return nil
-    
 end
-
