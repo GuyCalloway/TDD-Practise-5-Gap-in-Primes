5975a4584228cfdffbe7b18c90c042cedbcca928
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-21 16:21:23 +0100

    Optimisation Refactor: Found prime_check method thats much faster

--- lib/gap.rb
+++ lib/gap.rb
@@ -2,34 +2,39 @@ def gap(g, m, n)
     if g > n - m
         return nil
     elsif n - m == 1
-        return [1, 2]
-        
+        return [1, 2]     
     else
-        t = Time.now
        gap_1(g, m, n)
-       t2 = Time.now
-       p t - t2
     end
 end
 
 
-def is_prime?(num)
+def prime_check(num)
     return if num <= 1
     (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
   end
 
+  #ASK WHY THIS IS SO MUCH FASTER^^
+# def prime_check(x)
+#     return if x <= 1
+#     arr = [*2..Math.sqrt(x)]
+#     arr.none? {|z| if x % z == 0
+                  
+#     end}
+# end
+
 # def gap_1(g, m, n)
     
 #     x = m
-#     arr = []
+#     arr_primes = []
 #     until x == n + 1
-#         if is_prime?(x)
-#             arr << x
+#         if prime_check(x)
+#             arr_primes << x
 #             if arr.length != 1
-#               if arr[1] - arr[0] == g
-#                 return arr
+#               if arr_primes[1] - arr_primes[0] == g
+#                 return arr_primes
 #               end
-#                arr.shift
+#                arr_primes.shift
 #             end
 #         end
 #         x += 1
@@ -38,13 +43,14 @@ def is_prime?(num)
 # end
 
 def gap_1(g, m, n)
-    arr = [*m..n].select { |x| is_prime?(x)}     
+
+    arr_of_primes = [*m..n].select { |x| prime_check(x)}     
     i = 0
     j = 1
 
-    until arr[j] == nil
-        if g == arr[j] - arr[i]
-            return [arr[i],arr[j]]
+    until arr_of_primes[j] == nil
+        if g == arr_of_primes[j] - arr_of_primes[i]
+            return [arr_of_primes[i],arr_of_primes[j]]
         end
         i += 1
         j += 1
