5dd4c0cc93bfacd9e59f45b58c5a403771ec6f39
GuyCalloway <guyfcalloway@gmail.com>
2020-03-12 08:46:14 +0000

    Renamed variable

--- lib/gap_in_primes.rb
+++ lib/gap_in_primes.rb
@@ -1,15 +1,15 @@
-def gap_in_primes(g, m, n)
-  g > n - m ? nil : find_primes(g, m, n)
+def gap_in_primes(gap, start_val, end_val)
+  gap > end_val - start_val ? nil : find_primes(gap, start_val, end_val)
 end
 
-def find_primes(g, m, n)
-  num = m
+def find_primes(gap, start_val, end_val)
+  num = start_val
   array_of_primes = []
-  until num == n + 1
+  until num == end_val + 1
       if prime_check(num)
           array_of_primes << num
           if array_of_primes.length != 1
-            if array_of_primes[1] - array_of_primes[0] == g
+            if array_of_primes[1] - array_of_primes[0] == gap
               return array_of_primes
             end
              array_of_primes.shift
@@ -21,6 +21,6 @@ def find_primes(g, m, n)
 end
 
 def prime_check(num)
-  return num if num <= 1
+  return num if num == 1
   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
